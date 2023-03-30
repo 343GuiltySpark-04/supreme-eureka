96684983bc404cde13d9dcf281f6eb3837f63b38
Tristan Adams <killerdragonxs@gmail.com>
2023-03-30 11:50:54 -0400

    modified:   main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
-from sqlalchemy import (Column, Integer, MetaData, String, Table,
-                        create_engine, text)
+from typing import List, Optional
+
+from sqlalchemy import (Column, ForeignKey, Integer, MetaData, String, Table,
+                        create_engine, insert, text)
+from sqlalchemy.orm import (DeclarativeBase, Mapped, Session, mapped_column,
+                            relationship)
 
 engine = create_engine("sqlite+pysqlite:///:memory:", echo=True)
-
-
-with engine.connect() as conn:
-    result = conn.execute(text("select 'hello world'"))
-    print(result.all())
-    conn.commit()
 
 
 metadata_obj = MetaData()
@@ -19,3 +17,113 @@ user_table = Table(
     Column("name", String(30)),
     Column("fullname", String),
 )
+
+metadata_obj.create_all(engine)
+
+
+address_table = Table(
+    "address",
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("user_id", ForeignKey("user_account.id"), nullable=False),
+    Column("email_address", String, nullable=False),
+)
+
+
+with engine.connect() as conn:
+    result = conn.execute(text("select 'hello world'"))
+    print(result.all())
+
+
+with engine.connect() as conn:
+    conn.execute(text("CREATE TABLE some_table (x int, y int)"))
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 1, "y": 1}, {"x": 2, "y": 4}],
+    )
+    conn.commit()
+
+with engine.connect() as conn:
+    result = conn.execute(text("SELECT x, y FROM some_table"))
+    for row in result:
+        print(f"x: {row.x}  y: {row.y}")
+
+
+with engine.connect() as conn:
+    result = conn.execute(
+        text("SELECT x, y FROM some_table WHERE y > :y"), {"y": 2})
+    for row in result:
+        print(f"x: {row.x}  y: {row.y}")
+
+
+with engine.connect() as conn:
+    conn.execute(
+        text("INSERT INTO some_table (x, y) VALUES (:x, :y)"),
+        [{"x": 11, "y": 12}, {"x": 13, "y": 14}],
+    )
+    conn.commit()
+
+
+print("ORM EQV")
+print("------------------------------------------------------")
+
+stmt = text("SELECT x, y FROM some_table WHERE y > :y ORDER BY x, y")
+with Session(engine) as session:
+    result = session.execute(stmt, {"y": 6})
+    for row in result:
+        print(f"x: {row.x}  y: {row.y}")
+
+
+print("ORM COMMIT")
+print("--------------------------------------------------------")
+
+
+with Session(engine) as session:
+    result = session.execute(
+        text("UPDATE some_table SET y=:y WHERE x=:x"),
+        [{"x": 9, "y": 11}, {"x": 13, "y": 15}],
+    )
+    session.commit()
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+class User(Base):
+    __tablename__ = "user_account"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(30))
+    fullname: Mapped[Optional[str]]
+    addresses: Mapped[List["Address"]] = relationship(back_populates="user")
+
+    def __repr__(self) -> str:
+        return f"User(id={self.id!r}, name={self.name!r}, fullname={self.fullname!r})"
+
+
+class Address(Base):
+    __tablename__ = "address"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    email_address: Mapped[str]
+    user_id = mapped_column(ForeignKey("user_account.id"))
+    user: Mapped[User] = relationship(back_populates="addresses")
+
+    def __repr__(self) -> str:
+        return f"Address(id={self.id!r}, email_address={self.email_address!r})"
+
+
+sandy = User(name="sandy", fullname="Sandy Cheeks")
+squidward = User(name="squidward", fullname="Squidward Tentacles")
+krabs = User(name="ehkrabs", fullname="Eugene H. Krabs")
+
+
+Base.metadata.create_all(engine)
+
+session = Session(engine)
+
+session.add(squidward)
+session.add(krabs)
+
+session.flush()
+
+session.commit()
